9d1a1f27284f02e6dbbd17e599aad11660f794fa
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2025-04-23 11:00:34 -0300

    Update: Update Chart

--- src/services/chartService.py
+++ src/services/chartService.py
@@ -4,21 +4,24 @@ from typing import List
 from services.territoryService import Territory
 
 def create_chart(territories: List[Territory], chart_path:str):
+    if not territories:
+        logging.warning("No territories provided to create the chart.")
+        return
     try:
 
-        territoriesNames = []
-        territoriesDimensions = []
-        for territory in territories:
-            territoriesDimensions.append(territory.dimension)
-            territoriesNames.append(territory.name)
+        territoriesNames = [territory.name for territory in territories]
+        territoriesDimensions = [territory.dimension for territory in territories]
 
         plt.bar(territoriesNames, territoriesDimensions)
 
-        plt.title(f"{chart_path}")
+        plt.title(" x ".join(territoriesNames))
         plt.xlabel('territories')
         plt.ylabel('dimensions')
-        file_name = chart_path if chart_path.endswith(".png") else f"{chart_path}.png"
+        plt.yticks(territoriesDimensions)
+
+        file_name = f"{chart_path if chart_path.endswith('.png') else chart_path + '.png'}"
         plt.savefig(file_name, format="png")
+
         logging.info(f"Chart {chart_path} has been successfully created.")
     except Exception as error:
         logging.error(f"An unexpected error occurred while creating the chart {chart_path}: {error}")
